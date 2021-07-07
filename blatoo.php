a15bb52e3f700c166aeadc882140a1529926efb5
Phil Burton <philip.burton@chiaro.co.uk>
2021-07-07 10:58:18 +0100

    Improves formatting

--- blatoo.php
+++ blatoo.php
@@ -1,25 +1,23 @@
 <?php
-
     $version = "BlaGentoo 0.0.1";
 
-	function read_stdin()
-	{
-        	$fr=fopen("php://stdin","r");   // open our file pointer to read from stdin
-	        $input = fgets($fr,128);        // read a maximum of 128 characters
-	        $input = rtrim($input);         // trim any trailing spaces.
-        	fclose ($fr);                   // close the file handle
-	        return $input;                  // return the text entered
-	}
+    function read_stdin()
+    {
+       	$fr=fopen("php://stdin","r");   // open our file pointer to read from stdin
+        $input = fgets($fr,128);        // read a maximum of 128 characters
+        $input = rtrim($input);         // trim any trailing spaces.
+       	fclose ($fr);                   // close the file handle
+        return $input;                  // return the text entered
+    }
 
     $input = read_stdin();
     $input = explode(" ", $input, 10);
     $counter = 0;
     $url= "https://www.google.co.uk/#hl=en&sclient=psy-ab&q=site:forums.gentoo.org+OR+site:gentoo-wiki.com+OR+site:gentoo.org/doc";
     $cFlag = true;
-    while(sizeof($input) > $counter && $cFlag)
-    { 
-        switch($input[$counter])
-        {
+
+    while(sizeof($input) > $counter && $cFlag) { 
+        switch($input[$counter]) {
             case "-s":
             case "--solved":
                 $url .= "+solved";
@@ -41,8 +39,8 @@
                 break;
         }
     }
-    if($cFlag == true)
-    {
+
+    if ($cFlag == true) {
         echo $url . "\n";
     }
 ?>
